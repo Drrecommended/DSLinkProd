e0dd2036d9547c0695ba8b28e62ad4004a3346f4
Tyler French <tylerfrench89@gmail.com>
2022-08-05 15:54:18 -0700

    more backend

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const request = require('request')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-
 const app = express()
 
 //Middleware
@@ -14,6 +13,33 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.post('/subscribe', (req, res) => {
   const { email, js } = req.body
   console.log(req.body)
+
+  const mcData = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+      },
+    ],
+  }
+
+  const mcDataPost = JSON.stringify(mcData)
+
+  const options = {
+    url: '',
+    method: 'POST',
+    headers: {
+      Authorization: 'auth ..',
+    },
+    body: mcDataPost,
+  }
+
+  if (email) {
+    //success so far
+    
+  } else {
+    res.status(404).send({ message: 'Failed' })
+  }
 })
 
 const PORT = process.env.PORT || 5000
